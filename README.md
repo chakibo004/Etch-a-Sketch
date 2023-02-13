8c07a3cf653814051748a768c3363cf5147acc2b
chakibo004 <chakibheraoui4@gmail.com>
2023-02-13 22:33:38 +0100

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Etch-a-Sketch
 
 Etch a Sketch Project by : The Odin Project
+
+
+You will find: 
+    script.js
+    index.html
+    style.css
--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Creating the grid
 
-let Size=10;
+let Size=4;
+
+//Initializing with 16 elements
+
+draw(4);
+
+// Changing grid SIZE
 
 const gridSize=document.querySelector("#gridSize");
 gridSize.addEventListener("click",function(){
@@ -10,39 +16,105 @@ gridSize.addEventListener("click",function(){
     }else if(Size < 2){
         Size=2;
     }
-    draw(Size)
+    draw(Size);
 })
 
+// Function to draw the grid
 
 function draw(Size){
 
     const container=document.querySelector(".container");
-    container.innerHTML = "";
+    
     container.style.gridTemplateColumns = `repeat(${Size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${Size}, 1fr)`;
-
+    
 
     for (let i = 0;i<Size*Size;i++){
         let div= document.createElement("div");
+
         div.addEventListener("mouseover",function(){
-        div.classList.add("black");
+            div.style.backgroundColor = "black";
         });
+
         container.appendChild(div);
     }
 }
 
 
+// GOMME ( ERASER )
 
-/*
+const Eraser=document.querySelector(".Eraser");
+
+Eraser.addEventListener("click",function (){
+    const divs = container.querySelectorAll("div");
+    divs.forEach(div => {
+        div.addEventListener("mouseover", function() {
+            div.style.backgroundColor = "white";
+        });
+    })
+});
+
+
+// color picker
+
+const container = document.querySelector(".container");
+const colorInput = document.querySelector("input[type='color']");
+
+colorInput.addEventListener("input", function() {
+    const color = this.value;
+
+    const divs = container.querySelectorAll("div");
+    divs.forEach(div => {
+        div.addEventListener("mouseover", function() {
+            div.style.backgroundColor = color;
+        });
+    });
+});
+
+// Randomize PICTURE
+
+const Randomize = document.querySelector(".RandomColor");
 
 function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}*/
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+Randomize.addEventListener("click", function() {
+  const divs = document.querySelectorAll(".container div");
+  divs.forEach(div => {
+    div.style.backgroundColor = getRandomColor();
+  });
+});
+
+// Reset
+
+const Reset = document.querySelector(".Reset");
+
+Reset.addEventListener("click", function() {
+  const divs = document.querySelectorAll(".container div");
+  divs.forEach(div => {
+    div.style.backgroundColor = "white";
+  });
+});
+
+// Randomizer
+const Random = document.querySelector(".Randomizer");
+Random.addEventListener("click", function() {
+    const divs = document.querySelectorAll(".container div");
+    divs.forEach(div => {
+        div.addEventListener("mouseover", function() {
+            div.style.backgroundColor = getRandomColor();
+        });
+    });
+});
+  
+
+
 
 
 
